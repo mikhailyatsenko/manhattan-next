5cc24b9fdb73ea5af2281b6dd6edd4c2ba90bbe6
mikhailyatsenko <misha.ggg@gmail.com>
2026-02-14 12:31:10 +0100

    fix: fetch priority

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,14 @@ export const metadata: Metadata = {
   title: 'Студия красоты "Манхэттен beauty bar"',
   description:
     "Ногтевая студия в Москве, ТЦ «Триумфальный» (м. Профсоюзная, м. Университет). Маникюр и педикюр: классический, аппаратный, комбинированный, моделирование искусственных ногтей и многое другое.",
-  keywords: ["маникюр", "педикюр", "ногтевая студия", "Москва", "Триумфальный", "Профсоюзная"],
+  keywords: [
+    "маникюр",
+    "педикюр",
+    "ногтевая студия",
+    "Москва",
+    "Триумфальный",
+    "Профсоюзная",
+  ],
   openGraph: {
     title: 'Студия красоты "Манхэттен beauty bar"',
     description: "Ногтевая студия в Москве, ТЦ «Триумфальный»",
@@ -30,9 +37,24 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
+        />
         <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="preload"
+          as="image"
+          href="img/slide-1.webp"
+          fetchPriority="high"
+        />
       </head>
       <body className={sourceSerif.className}>{children}</body>
     </html>
